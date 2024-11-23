e6ffe2d551d153dcc3355f4d057f792cb1cec5e3
HappinessAndSadness <407916586@qq.com>
2024-11-23 18:34:28 +0800

    Initial commit

---
+++ app/src/main/java/com/example/experiment4_2/WebViewActivity.java
@@ -0,0 +1,40 @@
+package com.example.experiment4_2;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class WebViewActivity extends AppCompatActivity {
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_web_view);
+
+        webView = findViewById(R.id.webview);
+
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+        webView.loadUrl("https://www.baidu.com/");
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+}
